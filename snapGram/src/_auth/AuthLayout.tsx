fcd919092b268ee0f86eb918f30523b3164b55a1
amirsalamaa <amirsalama010@gmail.com>
2024-12-16 00:14:42 +0200

    snapgram project version 1

---
+++ snapGram/src/_auth/AuthLayout.tsx
@@ -0,0 +1,17 @@
+import { Outlet, Navigate } from "react-router-dom";
+function AuthLayout() {
+  const isAuth = false;
+  return (
+    <>
+      {isAuth ? (
+        <Navigate to="/" />
+      ) : (
+        <section className="flex flex-1 justify-center items-center flex-col py-10">
+          <Outlet />
+        </section>
+      )}
+    </>
+  );
+}
+
+export default AuthLayout;
